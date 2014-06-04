16886bd460c53f1f7b9ec7f75111b26b4ca78128
Virudson Thitilertdecha <virudson@gmail.com>
2014-06-04 16:42:54 +0700

    update unit serializer

--- app/serializers/unit_serializer.rb
+++ app/serializers/unit_serializer.rb
@@ -11,12 +11,25 @@ class UnitSerializer < ActiveModel::Serializer
   has_one :after_evo_unit
 
   def filter(keys)
+
+    # remove BB info if it not exists
+    unless object.brave_burst_title
+      keys.delete :brave_burst_title
+      keys.delete :brave_burst_details
+    end
+    unless object.super_brave_burst_title
+      keys.delete :super_brave_burst_title
+      keys.delete :super_brave_burst_details
+    end
+
     unless object.evolution_recipe
       keys.delete :evolution_recipe
       keys.delete :after_evo_unit
     end
 
+    keys.delete :leader_skill unless object.leader_skill
     keys.delete :before_evo_unit unless object.before_evo_unit
+
     keys
   end
 end
